776234023beae904a932069e720ff280d994fd6a
Maryam Heydari <m.heydari@asax.local>
2023-12-14 16:15:16 +0330

    check if the cartag is existed

--- Congestion.Application/CarService.cs
+++ Congestion.Application/CarService.cs
@@ -14,13 +14,18 @@ namespace Congestion.Application
         public async Task AddCarAsync(string tag, int carTypeId, CancellationToken ct)
         {
             var carType = await _repository.GetCarTypeByIdAsync(carTypeId);
-            if (carType == null) throw new Exception("carTypeType is not valid");
+            if (carType == null) 
+                throw new Exception("carType is not valid");
 
-            var car = new Car(tag, carType);          
-            await _repository.AddCarAsync(car);
+            var car = await _repository.GetCarByTagAsync(tag);
+            if (car!=null)
+            {
+                throw new Exception("this tag has been already existed ");
+            }          
+            await _repository.AddCarAsync(new Car(tag, carType));
 
         }
 
-       
+
     }
 }
